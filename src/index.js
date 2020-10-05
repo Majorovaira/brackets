a0b7c4d8f4afdfd7d90b3f2b46d3708b5f57fb7a
Majorovaira <majorovaira8@gmail.com>
2020-10-05 14:25:32 +0300

    init: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    for(let i = 0; i < bracketsConfig.length; i ++) {
+        if(str !== ''){
+        str = str.split(bracketsConfig[i].join('')).join('');
+    }
+}
+    for(i = 0; i < bracketsConfig.length; i ++){
+        if(str.includes(bracketsConfig[i].join(''))){
+            return check(str, bracketsConfig);
+        }
+    }
+  return str === '';
+
 }
